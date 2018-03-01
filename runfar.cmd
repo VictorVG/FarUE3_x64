11eb26ca39bed5031c66a8177fea252d8ad4ab2b
VictorVG <victorvg04@gmail.com>
2018-03-01 20:31:00 +0300

    Clear wiki branch

--- runfar.cmd
+++
@@ -1,2 +0,0 @@
-@echo off&if exist update.cmd (far\hidcon-x64.exe update.cmd)
-@cd Far&hidcon-x64.exe /detach rfar.cmd %1&exit
--- update.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d Far > nul
-if exist "plugins\intchecker\scripts\InChecker.GetFileHash.lua" (del /f/q "plugins\intchecker\scripts\InChecker.GetFileHash.lua" > nul)
-if exist "plugins\intchecker\scripts\intchecker_run.lua" (del /f/q "plugins\intchecker\scripts\intchecker_run.lua" > nul)
-if exist "plugins\farhints\Plugins \Folders" (rd /s/q "plugins\farhints\Plugins \Folders")
-far /import default.farconfig
-cd ../ > nul
-del /f/q update.cmd > nul
-exit /b 0 > nul
