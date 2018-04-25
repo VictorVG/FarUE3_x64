b78433899d37a955504e7b03746197f28feaa935
VictorVG <victorvg04@gmail.com>
2018-04-25 19:41:41 +0300

    b5196 Git-b9d0b41e1

--- runfar.cmd
+++ runfar.cmd
@@ -1,2 +1,3 @@
-@echo off&if exist update.cmd (far\hidcon-x64.exe update.cmd)
-@cd Far&hidcon-x64.exe /detach rfar.cmd %1&exit
+@echo off
+if exist %~dps0update.cmd (call %~dps0update.cmd > nul)
+cd Far && hidcon-x64.exe /detach rfar.cmd %1 && exit
