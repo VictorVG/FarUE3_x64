b102eca0ad9c068fffa6ce6ac6f5c696c95673d6
VictorVG <victorvg04@gmail.com>
2018-11-16 20:22:35 +0300

    b5317 Git-d4839498a

--- update.cmd
+++ update.cmd
@@ -2,11 +2,14 @@
 cd /d Far > nul
 if exist "plugins\intchecker\scripts\InChecker.GetFileHash.lua" (del /f/q "plugins\intchecker\scripts\InChecker.GetFileHash.lua" > nul)
 if exist "plugins\intchecker\scripts\intchecker_run.lua" (del /f/q "plugins\intchecker\scripts\intchecker_run.lua" > nul)
-if exist "plugins\farhints\Plugins\Folders" (rd /s/q "plugins\farhints\Plugins\Folders")
-if exist "plugins\sqlitedb" (rd /s/q "plugins\sqlitedb")
-if exist "Profile\PluginsData\FC97A376-00D9-4DE4-B2E1-BFDC3A8D8B0B.db" (del /s/q "Profile\PluginsData\FC97A376-00D9-4DE4-B2E1-BFDC3A8D8B0B.db")
-if exist "Profile\colors.db" (del /s/q "Profile\colors.db")
-if exist "Profile\highlight.db" (del /s/q "Profile\highlight.db")
+if exist "plugins\farhints\Plugins\Folders" (rd /s/q "plugins\farhints\Plugins\Folders" > nul)
+if exist "plugins\sqlitedb" (rd /s/q "plugins\sqlitedb" > nul)
+if exist "Profile\PluginsData\FC97A376-00D9-4DE4-B2E1-BFDC3A8D8B0B.db" (del /s/q "Profile\PluginsData\FC97A376-00D9-4DE4-B2E1-BFDC3A8D8B0B.db" > nul)
+if exist "Profile\colors.db" (del /s/q "Profile\colors.db" > nul)
+if exist "Profile\highlight.db" (del /s/q "Profile\highlight.db" > nul)
+if exist "Profile\Macros\scripts\Shell_DelTmp.lua" (del /s/q "Profile\Macros\scripts\Shell_DelTmp.lua" > nul)
+if exist "Profile\Macros\scripts\Dialog_ToEditor.lua" (del /s/q "Profile\Macros\scripts\Dialog_ToEditor.lua" > nul)
+if exist "Profile\Macros\scripts\Editor_IntegrateMacro.lua" (del /s/q "Profile\Macros\scripts\Editor_IntegrateMacro.lua" > nul)
 far /import default.farconfig
 cd ../ > nul
 del /f/q update.cmd > nul
