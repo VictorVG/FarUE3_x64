1ea64bada9cb62989174d04dbc006bbfb273fd0a
VictorVG <victorvg04@gmail.com>
2019-01-15 19:51:16 +0300

    b5351 Git-7af55dbd7

--- update.cmd
+++ update.cmd
@@ -5,7 +5,6 @@ set prm=%prm:~0,3% && set M=%prm:~0,1% && set N=%prm:~2,1%
 if %M% geq 6 (if %N% geq 1 (move /y Far\wget.ex7 Far\wget.exe > nul & move /y Far\wget.tx7 Far\wget.txt > nul))
 if exist Far\wget.*7 (del /f/q Far\wget.*7 > nul)
 cd /d Far > nul
-if exist "plugins\arclite\Formats\Forensic7z.*.format" (del /f/q plugins\arclite\Formats\Forensic7z.*.format > nul)
 if exist "plugins\sqlitedb" (rd /s/q "plugins\sqlitedb" > nul)
 if exist "plugins\dnd\holder_x86*.dnd"  (del /s/q "plugins\dnd\holder_x86*.dnd" > nul)
 if exist "plugins\farhints\Plugins\Folders" (rd /s/q "plugins\farhints\Plugins\Folders" > nul)
@@ -19,7 +18,7 @@ if exist "Profile\Macros\scripts\Shell_DelTmp.lua" (del /s/q "Profile\Macros\scr
 if exist "Profile\PluginsData\FC97A376-00D9-4DE4-B2E1-BFDC3A8D8B0B.db" (del /s/q "Profile\PluginsData\FC97A376-00D9-4DE4-B2E1-BFDC3A8D8B0B.db" > nul)
 rem far /import default.farconfig
 rem The next string used only if needed fix some Far or plug-in's settings, is another not used and always mast be comment!
-start /wait far "lua:mf.print('far /import farfix.cnf') Keys('Enter F10 Enter')" && del /f/q farfix.cnf > nul
+if exist farfix.cnf (start /wait far "lua:mf.print('far /import farfix.cnf') Keys('Enter F10 Enter')" && del /f/q farfix.cnf > nul)
 cd ../ > nul
 del /f/q update.cmd > nul
 exit /b 0 > nul
