b983e9230958cc25201396facec08201cb60d385
VictorVG <victorvg04@gmail.com>
2019-09-02 13:14:29 +0300

    b5462 Git-018efd475

--- recover.cmd
+++ recover.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal enableextensions
 rem Please, use this batch file only if have error on start'ing Far!
 echo ************************************************************************
 echo *                                                                      *
@@ -8,32 +9,26 @@ echo * Your filters and history will be deleted and all settings are reset! *
 echo *                                                                      *
 echo ************************************************************************
 echo Press any key for continue or Ctrl+C for cancel
+echo.
 pause
-copy /b .\Far\Profile\PluginsData\4EBBEFC8-2084-4B7F-94C0-692CE136894D.db /b %TEMP%
-cd /d .\Far\Profile
-del /f/q *.db*
-cd /d .\PluginsData
-del /f/q *.db*
-cd ../../
-setlocal
-set k=-ro-
-:for use Far on "Read Only" device just remove ":" on string :@set k=/ro
-:@set k=/ro
+copy /b "%~dp0Far\Profile\PluginsData\4EBBEFC8-2084-4B7F-94C0-692CE136894D.db" /b "%TEMP%"
+del /f/q "%~dp0Far\Profile\*.db*"
+del /f/q "%~dp0Far\Profile\PluginsData\*.db*"
 if defined PROCESSOR_ARCHITEW6432 (set reg="%systemroot%\sysnative\reg.exe"
      ) else (
      set reg=reg)
-echo REGEDIT4>%TEMP%\def.reg
-echo ; >>%TEMP%\def.reg
-echo [HKEY_CURRENT_USER\Software\Far Manager\Plugins\FarTray]>>%TEMP%\def.reg
-echo "Disable"=dword:00000000>>%TEMP%\def.reg"
-echo ; >>%TEMP%\def.reg"
-echo [HKEY_CURRENT_USER\Software\Far Manager\Plugins\MultiArc]>>%TEMP%\def.reg
-echo "DefaultFormat"="RAR">>%TEMP%\def.reg
-reg import %TEMP%\def.reg
-del /f/q %TEMP%\def.reg
-copy /b %TEMP%\4EBBEFC8-2084-4B7F-94C0-692CE136894D.db /b .\Far\Profile\PluginsData
-del /f/q %TEMP%\4EBBEFC8-2084-4B7F-94C0-692CE136894D.db
-cd ./Far
-far -import default.farconfig
-cd ../
-start /i .\Far\Far.exe %k
+echo REGEDIT4>"%TEMP%\def.reg"
+echo ; >>"%TEMP%\def.reg"
+echo [HKEY_CURRENT_USER\Software\Far Manager\Plugins\FarTray]>>"%TEMP%\def.reg"
+echo "Disable"=dword:00000000>>"%TEMP%\def.reg"
+echo ; >>"%TEMP%\def.reg"
+echo [HKEY_CURRENT_USER\Software\Far Manager\Plugins\MultiArc]>>"%TEMP%\def.reg"
+echo "DefaultFormat"="RAR">>"%TEMP%\def.reg"
+%reg% import "%TEMP%\def.reg"
+del /f/q "%TEMP%\def.reg"
+copy /b "%TEMP%\4EBBEFC8-2084-4B7F-94C0-692CE136894D.db" /b "%~dp0Far\Profile\PluginsData"
+del /f/q "%TEMP%\4EBBEFC8-2084-4B7F-94C0-692CE136894D.db"
+cd /d "%~dp0Far"
+start /wait far "lua:mf.print('far -import default.farconfig') Keys('Enter F10 Enter')"
+start /i Far C:\ C:\ -ro-
+exit /b
--- runfar.cmd
+++ runfar.cmd
@@ -1,3 +1,6 @@
 @echo off
+rem Usage: runfar [<apanel_patch> [<ppamel_patch> [<startup_key>]]]
+setlocal enableextensions
+set  rf="%~dp0Far\rfar.cmd"
 if exist "%~dp0update.cmd" (call "%~dp0update.cmd" > nul)
-cd Far && hidcon-x64.exe /detach rfar.cmd %1 && exit
+cd /d "%~dp0Far" && "%~dp0Far\hidcon-x64.exe" /detach "%rf% %1 %2 %3" && exit
