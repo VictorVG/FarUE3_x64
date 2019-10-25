87b1dd3d9db2a8d535dbb9b83f4ace4237d62ef2
VictorVG <victorvg04@gmail.com>
2019-10-25 15:40:15 +0300

    Delete unused

--- Far/Profile/Macros/scripts/Plugin_IntChecker.lua
+++ Far/Profile/Macros/scripts/Plugin_IntChecker.lua
@@ -64,33 +64,83 @@
 -- 07.10.2019 22:00:27 +0300
 -- v1.7.0 - считаем хэши для всех файлов в локальном каталоге и его подкаталогах, рефакторинг;
 -- 09.10.2019 19:01:24 +0300
+-- v1.8.0 - автовыбора режима счёта "папка/файл" для UNC пути с коррекцией ошибок ввода и
+-- проверкой их существания, хэши сохраняются в обрабатываемый UNC каталог, рефакторинг.
+-- 13.10.2019 00:02:14 +0300
+-- v1.8.1 - возвращаемый хэш всегда строка, все сообщения на английском, при ошибочном Target
+-- выводим хэш на экран, рефакторинг;
+-- 13.10.2019 16:32:37 +0300
 
 local ICId,ICMID="E186306E-3B0D-48C1-9668-ED7CF64C0E65","A22F9043-C94A-4037-845C-26ED67E843D1";
 local Mask="/.+\\.(md5|sfv|sha(1|3|256|512)|wrpl)/i";
 local MsB,MsF=Mouse.Button,Mouse.EventFlags;
-local Msg="Командая строка не пуста, но под курсором хэш файл. Что выполнить? Команду: Ok или Проверку: Cancel";
-local function chashex(hs,ft,md)
+local Msg="The command line is not empty, but a hash file is under the cursor.\nWhat to do? Command: Ok or Verification: Cancel";
+local NmMsg,md,fn,dn,err,nm="Please, input path to file or folder",0;
+
+local function PSplitU(p)
+
+-- UNC функция разбора пути к файлам или каталогам с коррекцией ошибок ввода,
+-- проверкой существования объекта и автовыбором режимов вызова chachex().
+--
+-- Логика анализатора:
+--
+-- если задан каталог, то считаем от него и dn == полный путь к нему, fn == его имя,
+-- если задан файл, то dn == путь к его родительскому каталогу,
+-- fn == его имя. Хэш файл если задана его запись всегда пишем в dn, а тот
+-- указываем с полным путём чтобы знать куда писать.
+--
+-- аргументы вызова:
+--
+-- p - разбираемый путь к объекту;
+--
+-- возвращает:
+--
+-- dn - UNC путь к его родительскому каталогу;
+-- fn - имя объекта для функции форматирования списка хэшей;
+-- mod - режим работы chashex();
+-- err - код ошибки:
+--     0 - объект существует, но его доступность не проверяется, значения
+--         dn, fn и mod действительны;
+--     1 - объект не существует, значения dn, fn и mod не действительны;
+  if (mf.substr(p,-1) == "/" or mf.substr(p,-1) == "\\") then p=mf.substr(p,0,-1) else p=tostring(p) end;
+   fn=mf.substr(p,mf.rindex(p,"\\") + 1);
+   if mf.fexist(p) then
+       if bit64.band(mf.fattr(p),16) == 16 then  md=3; err=0;
+         dn=p;
+        else
+         md=2; err=0;
+         dn=mf.substr(p,0,mf.rindex(p,"\\"));
+       end;
+      else
+       err=1; dn=tostring(""); fn=tostring(""); md=0;
+     end
+    return dn,fn,md,err
+end;
+
+local function chashex(hs,ft,md,nm,er)
 -- Функция не работает с виртуальными панелями плагинов!
 -- Возвращает посчитанный хэш или 0 при ошибке, или -1 если файл на
 -- виртуальной панели плагина;
+-- er - код ошибки ("флаг достоверности аргументов") возвращаемый PSplitU() или иным парсером путей;
 -- ft - формат 1 == BSD, 0 == GNU, по умолчанию GNU;
 -- hs - имя алгоритма;
 -- md - режим счёта:
 --    0 - файл под курсором,
 --    1 - выбранные файлы или весь каталог без учёта подкаталогов;
---    2 - указанный файл,
+--    2 - указанный файл;
 --    3 - указанный каталог и его подкаталоги;
 --    4 - локальный каталог и его подкаталоги;
+-- nm - имя файла или каталога для кого считать в режимах 2 и 3
 if APanel.Plugin then do return -1 end end;
-local s,sv,i,j=tostring(""),APanel.CurPos,0,0;
+local s,sv,i,j,name=tostring(""),APanel.CurPos,0,0,tostring("");
 local function ch(hn,pth)
 -- pth - UNC путь к проверяемому файлу;
 -- hsh - имя алгоритма;
-local ICId="E186306E-3B0D-48C1-9668-ED7CF64C0E65";
-   return mf.string(Plugin.SyncCall(ICId,"gethash",""..hn.."",""..pth..""));
+local ICId,Quiet="E186306E-3B0D-48C1-9668-ED7CF64C0E65",true;
+   return mf.string(Plugin.SyncCall(ICId,"gethash",""..hn.."",""..pth.."",1));
 end
  if md == 4 then
-  local nr,hr,lr,drr,dlr=tostring(""),tostring(""),0,far.GetDirList(APanel.Path),{};
+  local hr,lr,drr,dlr=tostring(""),tostring(""),0,far.GetDirList(APanel.Path),{};
    for i=1,#drr do
     dlr[i]=drr[i].FileName
      lr=mf.substr(dlr[i],#APanel.Path + 1)
@@ -98,27 +148,29 @@ end
      if hr ~= "0" then if ft == 1 then s=s..hs.." ("..lr..") = "..hr.."\n" else s=s..hr.." *"..lr.."\n" end end;
    end;
  end;
- if md == 3 then
-  local nm,hl=mf.prompt("Please, select folder","Path",5,nil),tostring("");
-   if mf.fexist(nm) == true then
-    local dr,dl=far.GetDirList(nm),{}
+ if err == 0 then
+    if md == 3 then
+     local hl,dr,dl=tostring(""),far.GetDirList(nm),{};
       for i=1,#dr do
        dl[i]=dr[i].FileName
        hl=ch(hs,dl[i])
-       if hl ~= "0" then if ft == 1 then s=s..hs.." ("..dl[i]..") = "..hl.."\n" else s=s..hl.." *"..dl[i].."\n" end end;
+       if hl ~= "0" then if ft == 1 then s=s..hs.." ("..mf.substr(dl[i],1 + #nm)..") = "..hl.."\n"
+         else s=s..hl.." *"..mf.substr(dl[i],1 + #nm).."\n" end
+        end;
       end
-   end;
- end;
- if md == 2 then local nm=mf.prompt("Please, select file","File",5,nil);
-  if ft == 1 then s=hs.." ("..nm..") = "..ch(hs,nm) else s=ch(hs,nm).." *"..nm end;
+     end;
+    if md == 2 then
+     if ft == 1 then s=hs.." ("..fn..") = "..ch(hs,nm) else s=ch(hs,nm).." *"..fn; end;
+    end;
  end;
  if md == 1  then
   if APanel.Selected then
   j=APanel.SelCount;
    for i=1,j,1 do Panel.SetPosIdx(0,i,1);
     if not APanel.Folder then
-     if ft == 1 then s=s..hs.." ("..APanel.Current..") = "..ch(hs,APanel.Current).."\n"
-       else s=s..ch(hs,APanel.Current).." *"..APanel.Current.."\n" end;
+     if ft == 1 then s=s..hs.." ("..APanel.Current..") = "..ch(hs,APanel.Current).."\n";
+       else s=s..ch(hs,APanel.Current).." *"..APanel.Current.."\n";
+     end;
     end
    end
    Panel.SetPosIdx(0,sv);
@@ -144,7 +196,7 @@ end
      end;
    end;
   end;
-  return mf.trim(s);
+  return mf.trim(tostring(s));
 end;
 
 Macro{
@@ -157,7 +209,7 @@ Macro{
   condition=function() return (mf.fmatch(APanel.Current,Mask)==1 and not (MsB==0x0001 and MsF==0x0001)) end;
   action=function()
   if not CmdLine.Empty then
- if mf.msgbox("Вопрос к пользователю:",Msg,0x00020000) == 1
+ if mf.msgbox("Question to the user:",Msg,0x00020000) == 1
         then
           Keys("Enter");
         else
@@ -184,14 +236,14 @@ Macro{
   id="A3C223FD-6769-4A6E-B7E6-4CC59DE7B6A2";
   area="Shell";
   key="AltG";
-  description="Integrity Checker: calc hash for the file under cursor";
+  description="Integrity Checker: calculate hash for ...";
   priority=50;
   condition=function() return not APanel.Plugin end;
   action=function()
-  local mod,tg,sem,hsum,hash,arg,hs,ext,md,ft="c","f",0,"hashlist","",2,"MD5","md5",0,0;
+  local mod,tg,sem,hsum,hash,arg,hs,ext,ft,name="c","f",0,"hashlist","",2,"MD5","md5",0,"";
   arg = mf.lcase(mf.prompt("1:CRC32;2:MD5;3:SHA1;4:SHA-256;5:SHA-512;6:SHA3-512;7:Whirlpool",nil,1,nil));
   tg = mf.lcase(mf.prompt("Target: D - display; C - Windows clipboard; F - hash file",nil,1,nil))
-  mod = mf.lcase(mf.prompt("A - all; C - current; D - UNC dir; F - UNC file; R - dir; S - sel;",nil,1,nil));
+  mod = mf.lcase(mf.prompt("A - All; C - Cur. file; D - Cur. dir; S - Sel. files; U - UNC path;",nil,1,nil));
   if mf.lcase(mf.prompt("Format: B - BSD UNIX; G - GNU",nil,1,nil)) == "b" then ft=1; else ft=0; end;
    if arg == "1" then hs="CRC32"; ext=".sfv";
      elseif arg == "2" then hs="MD5"; ext=".md5";
@@ -209,21 +261,57 @@ Macro{
               end;
            end;
         end;
-         if mod == "r" then md = 4; elseif mod == "d" then md = 3;
-           else if mod == "f" then md=2; elseif (mod == "a" or mod == "s") then md=1; else md=0; end end;
-     if  tg=="d" then far.Show(chashex(hs,ft,md))
-       elseif tg == "c" then
-        sem=mf.clip(5,1);
-          mf.clip(1,chashex(hs,ft,md));
-          if sem==2 then mf.clip(5,2) end;
-       else
-        if md == 0 then hsum=mf.fsplit(APanel.Current,4)..ext; else hsum=hsum..ext end;
-          if Panel.FExist(hsum,0) >=1 then Keys("ShiftF8 Enter") end;
-           hash=chashex(hs,ft,md);
-           local sum = io.open(hsum,"wb")
-          sum:write(hash);
-        sum:flush();
-      sum:close();
+ if mod == "u" then
+     name=mf.prompt(NmMsg,nil,5,nil);
+     PSplitU(name);
+   if err == 0 then
+    if md == 3 then
+      if tg == "f" then hsum=dn.."\\".."hashsum"..ext; end;
+     elseif md == 2 then
+     if tg == "f" then  hsum=dn.."\\"..mf.fsplit(fn,4)..ext; end;
     end;
+   else
+     far.Message("Integrity Checker: UNC path ".."\r"..name.."\r".."not exist or access denied or server is \nshutdown or user is not log-on!","Error","Ok","w",nil,nil)
+   end;
+  else
+   if mod == "d" then
+     md=4;
+     if tg == "f" then dn=APanel.Path0;
+     fn=APanel.Path0..hashsum..ext; end;
+     er=0;
+    elseif (mod == "a" or mod == "s") then
+     md=1;
+     if tg == "f" then
+        dn=APanel.Path0;
+        hsum=APanel.Path0.."hashsum"..ext;
+     end;
+     er=0;
+    else
+     md=0;
+     if tg == "f" then
+        dn=APanel.Path;
+        hsum=APanel.Path0..mf.fsplit(fn,4)..ext;
+     end;
+     er=0;
+   end;
+ end;
+  if tg == "d" then
+     far.Show(chashex(hs,ft,md,name,err))
+   elseif tg == "c" then
+     sem=mf.clip(5,1);
+     mf.clip(1,chashex(hs,ft,md,name,err));
+     if sem==2 then mf.clip(5,2) end;
+   else
+     if tg == "f" then
+      if Panel.FExist(hsum,0) >= 1 then win.DeleteFile(hsum) end;
+          hash=chashex(hs,ft,md,name,err);
+           local sum = io.open(hsum,"wb");
+          sum:write(hash);
+         sum:flush();
+       sum:close();
+     else
+       far.Show(chashex(hs,ft,md,name,err));
+     end;
+   end;
 end;
 }
--- Far/plugins/intchecker/scripts/IntChecker.GetFileHash.lua
+++ Far/plugins/intchecker/scripts/IntChecker.GetFileHash.lua
@@ -2,16 +2,17 @@
 --
 -- Параметры вызова: "команда", "алгоритм", "путь к файлу"
 -- Поддерживается пока одна команда "gethash" - получить хеш одного файла
--- Список доступных алгоритмов: crc32, md5, sha1, sha-256, sha-512, sha3-512, whirlpool
+-- Список доступных алгоритмов:
+-- crc32, md5, sha1, sha-256, sha-512, sha3-512, whirlpool
 -- Регистр в названии команд и в названии алгоритмов не учитывается.
--- Вызов возвращает расчитанный хеш файла.
+-- Вызов возвращает расчитанный хеш файла, сообщения об ошибках отключены.
 
 Macro {
   description="Calculate hash for the file under the cursor";
   area="Shell";
   key="CtrlShiftH";
   action=function()
-    far.Show (Plugin.SyncCall("E186306E-3B0D-48C1-9668-ED7CF64C0E65",
-              "gethash", "md5", APanel.Path0.."\\"..APanel.Current))
+  local Quiet,ICID=true,"E186306E-3B0D-48C1-9668-ED7CF64C0E65";
+  far.Show (Plugin.SyncCall(ICID,"gethash","md5",APanel.Path0.."\\"..APanel.Current,Quiet))
   end;
 }
