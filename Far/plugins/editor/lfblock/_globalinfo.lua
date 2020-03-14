b9df645741deccb0002b88d0ecbd2a519fb98976
VictorVG <victorvg04@gmail.com>
2020-03-14 08:46:05 +0300

    b5570 Git-47d4527aa

--- Far/plugins/editor/lfblock/_globalinfo.lua
+++ Far/plugins/editor/lfblock/_globalinfo.lua
@@ -1,7 +1,7 @@
 function export.GetGlobalInfo()
   return {
-    Version       = { 2020, 03, 12,    1 },
-    MinFarVersion = {    3,  0,  0, 4040 },
+    Version       = { 2020, 03, 12,    2 },
+    MinFarVersion = {    3,  0,  0, 5400 },
     Guid          = win.Uuid("23C16506-38EE-48D1-9874-CD1736408867"),
     Title         = "LUA Format Block",
     Description   = "LUA Format Block",
--- Far/plugins/lflist/_globalinfo.lua
+++ Far/plugins/lflist/_globalinfo.lua
@@ -1,7 +1,7 @@
 function export.GetGlobalInfo()
   return {
-    Version       = { 2020, 03, 12,    1 },
-    MinFarVersion = {    3,  0,  0, 4040 },
+    Version       = { 2020, 03, 12,    2 },
+    MinFarVersion = {    3,  0,  0, 5400 },
     Guid          = win.Uuid("0785F214-65B9-47B6-B9D8-F24FADC60AA0"),
     Title         = "LUA File List",
     Description   = "File list creator",
