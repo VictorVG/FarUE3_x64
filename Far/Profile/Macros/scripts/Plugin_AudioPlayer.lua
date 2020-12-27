f638b30fb38a3fafb0ed49ff49a4c668bcb5f63a
VictorVG <victorvg04@gmail.com>
2020-12-27 06:37:07 +0300

    Delete unused

--- Far/Profile/Macros/scripts/Plugin_AudioPlayer.lua
+++ Far/Profile/Macros/scripts/Plugin_AudioPlayer.lua
@@ -1,32 +1,12 @@
-﻿-- HaRT, Sat 26 May, 2018 00:13
--- https://forum.farmanager.com/viewtopic.php?p=149864#p149864
--- Fixed message box missing flags definition, small refactoring. /VictorVG/
--- v1.01
+﻿-- v1.0.0, Initial release
+-- VictorVG @ VikSoft.Ru, 26.12.2020 05:32:35 +0300
 
-local APUUID = '9C3A61FC-F349-48E8-9B78-DAEBD821694B'
-local MSGFLG = '0x00010008'
-
-Macro {
-  description = "AudioPlayer: key bindings help by F1";
-  area = 'Dialog';
-  key = 'F1';
-  condition = function() return Dlg.Id == APUUID end;
-  action = function()
-     msgbox( 'Key bindings', [[
-
-Space,            VK_PLAY_PAUSE
-Enter           Next|Play/Pause
-Home               Rev to start
-Shift-/Left           Rev 5s/5%
-Shift-/Right          FF  5s/5%
-Ctrl Left , PgUp,  VK_PLAY_NEXT
-Ctrl Right, PgDn,  VK_PLAY_PREV
-Up                 Volume Up
-Down               Volume Down
-Esc                Stop & close
-Ctrl PgUp/PgDn       First/Last
-Ctrl Home/End        First/Last
-
-]], MSGFLG )
-  end
+Macro{
+ id="942B3B5B-2994-4698-9D3F-03CE80686454";
+ area="Shell";
+ key="Enter NumEnter MsM1Click";
+ description="AudiPlayer";
+ priority=60;
+ condition=function() return (mf.fmatch(APanel.Current,"/.+\\.(web(a|m))/i")==1) end;
+ action=function() Plugin.Call("9C3A61FC-F349-48E8-9B78-DAEBD821694B",APanel.Current) end;
 }
