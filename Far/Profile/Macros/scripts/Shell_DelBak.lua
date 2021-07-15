7431e031573a9b43e79e67b139090b9d5ea60c42
VictorVG <victorvg04@gmail.com>
2021-07-15 17:31:43 +0300

    b5849 Git-775633e75

--- Far/Profile/Macros/scripts/Shell_DelBak.lua
+++ Far/Profile/Macros/scripts/Shell_DelBak.lua
@@ -1,75 +1,57 @@
-﻿-------------------------------------------------------------------------------
--- Удаление резервных и временных файлов. © SimSU
--------------------------------------------------------------------------------
--- Мелкий рефакторинг VictorVG, 16.11.2018 04:40:47 +0300
----- Настройки
-local function Settings()
--- Начало файла Profile\SimSU\Shell_DelBak.cfg
-return{
-  {id="23264cfd-e9a7-4acd-8c23-5136ce32a33a";
-  Key="AltB"; --Prior=50; --Sort=50;
-  Mask="/.+\\.(bak\\d*)$/i"; -- Маска для старых файлов (регулярное выражение).
-  };
-  {id="c7eda6df-3e65-4eec-b50a-7bc6d4dd8ce1";
-  Key="AltT"; --Prior=50;  --Sort=50;
-  Mask="/.+\\.(log|tmp|temp|(~\\w?|\\s?)tmp(~|\\s?)|\\w?log|dir|xml|c|h|(c|h)pp|txt|(h|i)\\.(\\w?|\\s?|(c|h)pp|c|h|rc))$/i"; -- Маска для временных файлов (регулярное выражение).
-  };
+﻿-- Shell_DelBak.lua - script for delete temporary and backup unused files by mask.
+-- VictorVG, VikSoft.Ru©, Russia, Moscow. All Right Reserved. 1996 - 2021
+-- 
+-- Script used configuaration variables: Msk1, Msk2, Excl .
+--
+-- Include Msk1 mask (PCRE regexp) is backup files mask,
+-- Include Msk2 mask (PCRE regexp) is temporary file type mask
+-- Exclude Excl mask (PCRE regexp) protected filetype;
+-- 
+-- v1.0 Initial release
+-- 08.07.2021 18:55:41 +0300
+-- v1.1, Update clear()
+-- 14.07.2021 03:21:58 +0300
+-- v1.2, Refactoring
+-- 14.07.2021 19:16:17 +0300
+-- v1.3, Use API for redraw panel, refactoring
+-- 15.07.2021 01:47:01 +0300
+
+
+local Msk1,Excl = "/.+\\.(ba(c|k))$/i","/.+\\.(bat|btm|cmd|lua|moon|whs|ps1)$/i";
+local Msk2 = "/.+\\.(log|temp|lock|(\\~|\\w?{1})tmp|(\\~|\\w?{1})log|dir|xml|c|h|(c|h)pp|svn|txt|rc)$/i";
+
+local function fd(Mask)
+  local Q,T,p = "\nDo you like delete all (%d units) files?","Delete unused files";
+   Panel.Select(0,1,3,Mask)
+   Panel.Select(0,0,3,Excl)
+   if APanel.Selected and msgbox(T,Q:format(APanel.SelCount),0x20000)==1 then
+    p =  Panel.SetPosIdx(0,0)
+   for j=1,APanel.SelCount do
+    Panel.SetPosIdx(0,j,1)
+    win.DeleteFile(APanel.Current)
+   end
+    panel.UpdatePanel(nil,1)
+    panel.RedrawPanel(nil,1)
+    Panel.SetPosIdx(0,p)
+   end
+  end;
+
+Macro{
+  id="3CCFF979-B268-4983-A00E-C3003086807A";
+  area="Shell";
+  key="AltB";
+  description="Delete backup files";
+  priority=60;
+  condition=function() return not APanel.Empty end;
+  action=function() fd(Msk1) end;
 }
--- Конец файла Profile\SimSU\Shell_DelBak.cfg
-end
 
----- Локализация
-_G.far.lang=far.lang or win.GetEnv("farlang")
--- Встроенные языки / Built-in laguages
-local function Messages()
-if far.lang=="Russian" then
--- Начало файла Profile\SimSU\Shell_DelBakRussian.lng
-return{
-  Descr="Удаление резервных и временных файлов. © SimSU";
-  Title="Удаление резервных и временных файлов";
-  Question="Вы хотите удалить все (%d шт.) файлов?";
+Macro{
+  id="E446E62C-1BC3-4CDF-8E54-1A5C1FF32663";
+  area="Shell";
+  key="AltT";
+  description="Delete temporary files";
+  priority=60;
+  condition=function() return not APanel.Empty end;
+  action=function() fd(Msk2) end;
 }
--- Конец файла Profile\SimSU\Shell_DelBakRussian.lng
-else--if far.lang=="English" then
--- Begin of file Profile\SimSU\Shell_DelBakEnglish.lng
-return{
-  Descr="Removal of backup and temporary files. © SimSU";
-  Title="Removal of backup and temporary files";
-  Question="You want to remove all (%d units) files?";
-}
--- End of file Profile\SimSU\Shell_DelBakEnglish.lng
-end end
-
--------------------------------------------------------------------------------
-local M=(loadfile(win.GetEnv("FARPROFILE").."\\SimSU\\Shell_DelBak"..far.lang..".lng") or Messages)()
-local S=(loadfile(win.GetEnv("FARLOCALPROFILE").."\\SimSU\\Shell_DelBak.cfg") or loadfile(win.GetEnv("FARPROFILE").."\\SimSU\\Shell_DelBak.cfg") or Settings)()
-
-local SimSU=_G.SimSU or {}
--------------------------------------------------------------------------------
-local Mask="/.+\\.(bak\\d*)$/i"
-local _
-local ok, mod = pcall(require,"SimSU.Shell_RememberSelected"); if ok then SimSU.Shell_RememberSelected=mod.Shell_RememberSelected end
-
-local function Shell_DelBak(mask)
-  mask = mask or Mask
-  _ = SimSU.Shell_RememberSelected and SimSU.Shell_RememberSelected.Save and SimSU.Shell_RememberSelected.Save(APanel)
-  Panel.Select(0); Panel.Select(0,1,3,mask) -- Выделяем по маске bak файлы.
-  if APanel.Selected and msgbox(M.Title,M.Question:format(APanel.SelCount),0x20000)==1 then
-    Keys("ShiftDel Enter")
-  end
-  _ = SimSU.Shell_RememberSelected and SimSU.Shell_RememberSelected.RestoreLater and SimSU.Shell_RememberSelected.RestoreLater(APanel)
-  return true
-end
--------------------------------------------------------------------------------
-local function filename() return Shell_DelBak() end
--------------------------------------------------------------------------------
-if _filename then return filename(...) end
-if not Macro then return {Shell_DelBak=Shell_DelBak} end
-SimSU.Shell_DelBak=Shell_DelBak; _G.SimSU=SimSU
--------------------------------------------------------------------------------
-for i=1,#S do
-  Macro {id=S[i].id;
-    area="Shell"; key=S[i].Key; priority=S[i].Prior; sortpriority=S[i].Sort; description=M.Descr;
-    action=function() return Shell_DelBak(S[i].Mask) end;
-  }
-end
